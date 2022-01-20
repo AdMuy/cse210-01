503bac4eace42a3f40be1cdba45ea035b6c5cef6
AdMuy <agu21010@byui.edu>
2022-01-19 22:43:07 -0600

    Loop works and score ready.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -2,60 +2,69 @@ def main():
     action = " "
     turn = True
     player = "X"
-    x_player_selection = [ ]
-    o_player_selection = [ ]
     options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    while action != False:
+    while not (match_win(options)):
         action = int(input(f"{player}'s turn. Please select a number 1-9: "))
-        if turn == True:
-            player = "O"
-            x_player_selection.append(action)
-            turn = False
-        elif turn == False:
+        main_options = match_options(player, action, options)
+        start_game(main_options)
+        # It calles the match_win function to confirm if it's True to stop the loop and 
+        # get the current player who won, instead of passing the turn to the other player
+        # and get the incorrect one on the congratulations message.
+        if match_win(options) == True:
+            break
+        # It calls the turns fuction to determine the next player.
+        who_turn = turns(turn)
+        player = who_turn[0]
+        turn = who_turn[1]
+    if match_win(options) == True:
+        print(f'Congratulations {player} you win!')
+    print (f'{match_win(options)}')
+
+def turns(turn):
+    if turn == False:
             player = "X"
-            o_player_selection.append(action)
             turn = True
+    elif turn == True:
+            player = "O"
+            turn = False
+    return player, turn
 
-        for selection in x_player_selection:
-            print (f'X selection: {selection}')
-            for option in options:
-                if option == selection:
-                    options[selection - 1] = "X"
-                # option = "X"
-        for selection in o_player_selection:
-            print (f'O Selection: {selection}')
-            for option in options:
-                if option == selection:
-                    options[selection - 1] = "O"
-                # option = "O"
-        match = (f'Match: \
-            \n{options[0]}|{options[1]}|{options[2]} \
-            \n-+-+- \
-            \n{options[3]}|{options[4]}|{options[5]} \
-            \n-+-+- \
-            \n{options[6]}|{options[7]}|{options[8]}')
-        yes = match_win(match, player, options, action)
-        print (f'{yes}')
-        action = yes
+def start_game(options):
+    print()
+    print(f"{options[0]}|{options[1]}|{options[2]}")
+    print('-+-+-')
+    print(f"{options[3]}|{options[4]}|{options[5]}")
+    print('-+-+-')
+    print(f"{options[6]}|{options[7]}|{options[8]}")
+    print()
 
-def match_win(match, player, options, action):
-    winner_board = (f'Match: \
-            \nX|X|X \
-            \n-+-+- \
-            \n{options[3]}|{options[4]}|{options[5]} \
-            \n-+-+- \
-            \n{options[6]}|{options[7]}|{options[8]}')
-            
-    if match == winner_board:
-        print(f'Congratulations, {player} you win.')
-        action = False
-        print(f'{match}')
-        return action
-    else:
-        action = True
-        print(f'{match}')
-        return action
+def match_options(player_turn, selection, options):
+    if player_turn == "X":
+        print (f'X selection: {selection}')
+        for option in options:
+            if option == selection:
+                options[selection - 1] = "X"
+                return options
+    if player_turn == "O":
+        print (f'O Selection: {selection}')
+        for option in options:
+            if option == selection:
+                options[selection - 1] = "O"
+                return options
+
+def match_win(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6])
         
+
+
+
 
 if __name__ == "__main__":
     main()
