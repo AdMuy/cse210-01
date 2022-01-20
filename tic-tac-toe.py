f5074a6985b223327cadd74a6129a05b2c40fc9b
AdMuy <agu21010@byui.edu>
2022-01-19 22:56:17 -0600

    Added comments on fuctions and finishing touches

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,23 +4,30 @@ def main():
     player = "X"
     options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     while not (match_win(options)):
+        # Request the selection from the user.
         action = int(input(f"{player}'s turn. Please select a number 1-9: "))
-        main_options = match_options(player, action, options)
-        start_game(main_options)
+        # Calls the match_options funtion to record the selected number by the current player.
+        # It passes the match_options result through the start_game function to print the game. 
+        start_game(match_options(player, action, options))
         # It calles the match_win function to confirm if it's True to stop the loop and 
         # get the current player who won, instead of passing the turn to the other player
         # and get the incorrect one on the congratulations message.
         if match_win(options) == True:
             break
         # It calls the turns fuction to determine the next player.
-        who_turn = turns(turn)
-        player = who_turn[0]
-        turn = who_turn[1]
-    if match_win(options) == True:
-        print(f'Congratulations {player} you win!')
-    print (f'{match_win(options)}')
+        whose_turn = turns(turn)
+        player = whose_turn[0]
+        turn = whose_turn[1]
+    # It displays a congratulations message when the match is over.
+    print(f'Congratulations {player} you win!')
 
 def turns(turn):
+    """Decides whose turns is it based on the last player's movement. 
+
+    Parameters
+        turn: a variable stating who made the last movement. 
+    Return: the next player to make a move.
+    """
     if turn == False:
             player = "X"
             turn = True
@@ -30,6 +37,11 @@ def turns(turn):
     return player, turn
 
 def start_game(options):
+    """Print the game, and replace the numbers with the player's name. 
+
+    Parameters
+        options: the list of movements made by the players.
+    """
     print()
     print(f"{options[0]}|{options[1]}|{options[2]}")
     print('-+-+-')
@@ -39,6 +51,13 @@ def start_game(options):
     print()
 
 def match_options(player_turn, selection, options):
+    """Changes the number in the options list to the player's name. 
+
+    Parameters
+        player_turn: the of the current player. 
+        selection: the number selected to be replaced.
+        options: the list of movements made by the players. 
+    """
     if player_turn == "X":
         print (f'X selection: {selection}')
         for option in options:
@@ -52,19 +71,20 @@ def match_options(player_turn, selection, options):
                 options[selection - 1] = "O"
                 return options
 
-def match_win(board):
-    return (board[0] == board[1] == board[2] or
-            board[3] == board[4] == board[5] or
-            board[6] == board[7] == board[8] or
-            board[0] == board[3] == board[6] or
-            board[1] == board[4] == board[7] or
-            board[2] == board[5] == board[8] or
-            board[0] == board[4] == board[8] or
-            board[2] == board[4] == board[6])
-        
+def match_win(game):
+    """Decides who won the game. 
 
-
-
+    Parameters
+        game: the current game. 
+    """
+    return (game[0] == game[1] == game[2] or
+            game[3] == game[4] == game[5] or
+            game[6] == game[7] == game[8] or
+            game[0] == game[3] == game[6] or
+            game[1] == game[4] == game[7] or
+            game[2] == game[5] == game[8] or
+            game[0] == game[4] == game[8] or
+            game[2] == game[4] == game[6])
 
 if __name__ == "__main__":
     main()
